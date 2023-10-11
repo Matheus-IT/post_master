ed361308273e5d74bf88f1c09d966c303eb73859
matheuscosta <matheuscosta.tech@gmail.com>
2023-10-10 23:13:31 -0300

    Code example for instagrapi

---
+++ main.py
@@ -0,0 +1,7 @@
+from instagrapi import Client
+
+cl = Client()
+cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+
+user_id = cl.user_id_from_username(ACCOUNT_USERNAME)
+medias = cl.user_medias(user_id, 20)
