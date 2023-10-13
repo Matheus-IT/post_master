0ed7d49ce81abe0b74f5d0922152dfadea12391e
matheuscosta <matheuscosta.tech@gmail.com>
2023-10-12 22:22:34 -0300

    Uploading post to instagram

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import os
 from instagrapi import Client
+from dotenv import load_dotenv
+from utils import change_aspect_ratio
+
+
+load_dotenv()
 
 cl = Client()
-cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+cl.login(os.getenv('INSTAGRAM_USERNAME'), os.getenv('INSTAGRAM_PASSWORD'))
 
-user_id = cl.user_id_from_username(ACCOUNT_USERNAME)
-medias = cl.user_medias(user_id, 20)
+user_id = cl.user_id_from_username(os.getenv('INSTAGRAM_USERNAME'))
+
+image_path = 'images/Pasted image.jpeg'
+caption = 'content'
+
+change_aspect_ratio(image_path)
+
+cl.photo_upload(image_path, caption)
+
+print('user_id', user_id)
