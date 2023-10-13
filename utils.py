6c08152e271015fb6ec185eb63b382fbf2f1bced
matheuscosta <matheuscosta.tech@gmail.com>
2023-10-12 22:22:11 -0300

    Utility function to change aspect ratio

---
+++ utils.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+
+def change_aspect_ratio(image_path):
+    # Open the image
+    image = Image.open(image_path)
+
+    # Calculate the new height to achieve a 4:5 aspect ratio
+    new_width = int(image.width * 4 / 5)
+    new_height = int(image.height * 5 / 4)
+
+    # Resize the image to the new aspect ratio
+    image = image.resize((new_width, new_height))
+
+    # Save the modified image
+    image.save(image_path)
+
+    return image
