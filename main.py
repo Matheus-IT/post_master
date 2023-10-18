beac14d0ec126b77c8df6c5a0d8b7ec0233320fd
matheuscosta <matheuscosta.tech@gmail.com>
2023-10-17 23:49:51 -0300

    Grabbing code example for linkedin

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 from instagrapi import Client
 from dotenv import load_dotenv
 from utils import change_aspect_ratio
+import requests
 
 
 load_dotenv()
 
+# post on instagram
 cl = Client()
 cl.login(os.getenv('INSTAGRAM_USERNAME'), os.getenv('INSTAGRAM_PASSWORD'))
 
@@ -19,3 +21,36 @@ change_aspect_ratio(image_path)
 cl.photo_upload(image_path, caption)
 
 print('user_id', user_id)
+
+# post on linkedin
+profile_id = 'YOUR_PROFILE_ID'
+
+#scope: w_member_social,r_liteprofile
+access_token = 'YOUR_ACCESS_TOKEN'
+
+url = "https://api.linkedin.com/v2/ugcPosts"
+
+headers = {'Content-Type': 'application/json',
+           'X-Restli-Protocol-Version': '2.0.0',
+           'Authorization': 'Bearer ' + access_token}
+
+
+post_data = {
+    "author": "urn:li:person:"+profile_id,
+    "lifecycleState": "PUBLISHED",
+    "specificContent": {
+        "com.linkedin.ugc.ShareContent": {
+            "shareCommentary": {
+                "text": "Hello World! This is my first Share on LinkedIn!!"
+            },
+            "shareMediaCategory": "NONE"
+        }
+    },
+    "visibility": {
+        "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+    }
+}
+
+response = requests.post(url, headers=headers, json=post_data)
+
+print(response)
